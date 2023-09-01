fe6f70c285002bb6386a46c7298d23509040325b
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-09-01 17:19:39 +0300

    Update insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Mr.Chocolate', 5,'M', 'Brown', 'Friendly',1);
+
+
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Rowdy',3, 'M', 'Black', 'Playful',1);
+
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Tabitha',7, 'F', 'Gray', 'Calm',1);
+
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Sergeant Brown' ,8, 'M', 'Brown', 'Brave',1);
+
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Melissa' , 4, 'F','White','Sweet',1);
+
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Grinch', 2, 'M', 'Green', 'Mischievous', 1);
+
+
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Wendy', 6, 'F', 'Brown', 'Energetic',1);
+
+INSERT INTO bears (name, age, sex, color,temperament, alive)
+VALUES('Unnamed Bear',NULL, NULL, NULL, NULL,NULL);
